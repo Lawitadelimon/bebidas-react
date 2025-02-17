1d25bde8b7b4cf1b6beb692cfd583611c3356d54
Lawitadelimon <lau72abrvjua@gmail.com>
2025-02-17 13:27:51 -0600

    07 show favorites in FavoritePage

--- src/pages/FavoritePage.tsx
+++ src/pages/FavoritePage.tsx
@@ -1,5 +1,31 @@
+import { useMemo } from "react"
+import { useAppStore } from "../stores/useAppStore"
+import DrinkCard from "../componentes/DrinkCard"
+
 export default function FavoritePage(){
+
+    const favorites = useAppStore(state => state.favorites)
+    const hasFavorites = useMemo(() => favorites.length > 0, [favorites])
     return (
-        <h1>Favorite Page</h1>
+        <>
+                <h1 className="text-6xl font-extrabold">Recetas</h1>
+                {
+                    hasFavorites? (
+                        <>
+                        <div className="grid grid-cols-1 md:grid-cols-4 2xl:grid-cols-3 my-10 gap-10">
+                            {
+                                favorites.map(drink => (
+                                    <DrinkCard 
+                                    drink={drink}
+                                    key={ drink.idDrink }/>
+                                ))
+                            }
+                        </div>
+                        </>
+                    ): (
+                        <p>Aqui se muestran los favoritos, por el momento no tienes</p>
+                    )
+                }
+                </>
     )
 }
---
+++ src/stores/favoritesSlice.ts
@@ -0,0 +1,34 @@
+import { StateCreator } from "zustand"
+import { Recipe } from "../types"
+
+export type FavoritesSliceType = {
+    favorites: Recipe[]
+    addFavorites: (recipe: Recipe) => void
+    favoriteExist: (id: Recipe['idDrink']) => boolean
+    loadFavorites: () => void
+}
+
+export const createFavoritesSlice: StateCreator<FavoritesSliceType> = (set, get) => ({
+    favorites: [],
+    addFavorites: (recipe) =>{
+        if(get().favorites.some(d => d.idDrink === recipe.idDrink)){
+            set({
+                favorites: [...get().favorites.filter(d => d.idDrink !== recipe.idDrink) ]
+            })
+        }else{
+            set({
+                favorites: [...get().favorites, recipe]
+            })
+        }
+        localStorage.setItem('favorites', JSON.stringify(get().favorites))
+    }, 
+    favoriteExist: (id) => {
+        return !get().favorites.some(d => d.idDrink === id)
+    },
+    loadFavorites: () => {
+        const dataStorage = localStorage.getItem('favorites')
+        set({
+            favorites: dataStorage? JSON.parse(dataStorage): []
+        })
+    }
+})
